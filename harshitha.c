f25afd950468f0125261983c1ddddc5374a78617
Harshitha S <harshithah785@gmail.com>
2025-05-13 15:42:11 +0530

    Create harshitha.c

---
+++ harshitha.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int shridevi()
+{
+    printf("harshitha\n");
+    printf("date of birth\n");
+    printf("age\n");
+    printf("mobile number\n");
+}
+int main()
+{
+    shridevi();
+    return 0;
+}
